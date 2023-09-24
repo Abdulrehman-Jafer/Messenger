8f566e3ae72021b377f98cb128b63af839892a11
Abdulrehman Jafer <abdulrehmanjaferworks01233@gmail.com>
2023-09-24 15:15:27 +0500

    styled contacts screen

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,22 +1,32 @@
-import { useNavigate } from "react-router-dom"
-import { User } from "../redux/features/user-slice"
-import { fixImageUrl } from "../utils/misc"
+import { useNavigate } from "react-router-dom";
+import { User } from "../redux/features/user-slice";
+import { fixImageUrl } from "../utils/misc";
 type contactProps = {
-    _id: string,
-    contact: Partial<User>,
-    saved_as: string,
-}
+  _id: string;
+  contact: Partial<User>;
+  saved_as: string;
+};
 
 export default function Contact({ contact, saved_as, _id }: contactProps) {
-    const image_src = fixImageUrl(contact.image!)
-    const navigate = useNavigate()
+  const image_src = fixImageUrl(contact.image!);
+  const navigate = useNavigate();
 
-    return (
-        <section onClick={() => navigate(`/contacts/${_id}`)} className="cursor-pointer hover:bg-pink-red hover:text-white">
-            <div className="flex gap-5 items-center p-2 border">
-                <img src={image_src} alt="cotact_image" className="flex-shrink-0 h-10 w-10 rounded-full " />
-                <p className="flex-1">{saved_as}</p>
-            </div>
-        </section>
-    )
+  return (
+    <section
+      onClick={() => navigate(`/contacts/${_id}`)}
+      className="cursor-pointer hover:bg-pink-red hover:text-white w-full"
+    >
+      <div className="flex gap-5 items-center p-2 border-2 border-blue-gray-100 rounded-md">
+        <img
+          src={image_src}
+          alt="cotact_image"
+          className="flex-shrink-0 h-10 w-10 rounded-full "
+        />
+        <div>
+          <p className="flex-1 font-[500]">{saved_as}</p>
+          <p className="text-grayish">{contact.public_number}</p>
+        </div>
+      </div>
+    </section>
+  );
 }
--- client/src/screens/Contacts.tsx
+++ client/src/screens/Contacts.tsx
@@ -1,46 +1,103 @@
-import { useTypedSelector } from "../redux/store"
-import { initializeContacts } from "../redux/features/contact-slice"
-import { BsSearch } from "react-icons/bs"
-import { useGetContactsQuery } from "../redux/service/api"
-import Contact from "../components/Contact"
-import { AiOutlinePlus } from "react-icons/ai"
-import Add_Contact from "../modals/Add_Contact"
-import { useState } from "react"
-import Navbar from "../components/Navbar"
-import useDispatchOnLoad from "../hooks/useDispatchOnLoad"
+import { useTypedSelector } from "../redux/store";
+import { initializeContacts } from "../redux/features/contact-slice";
+import { BsSearch } from "react-icons/bs";
+import { useGetContactsQuery } from "../redux/service/api";
+import Contact from "../components/Contact";
+import { AiOutlinePlus } from "react-icons/ai";
+import Add_Contact from "../modals/Add_Contact";
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import useDispatchOnLoad from "../hooks/useDispatchOnLoad";
+import no_data_found from "../assets/no_data_found.png";
 
 export default function Contacts() {
-    const [isModalOpen, setIsModalOpen] = useState(false)
-    const User = useTypedSelector((selector) => selector.userReducer)
-    const contactReducer = useTypedSelector(selector => selector.contactReducer)
-    const [contactsLoading] = useDispatchOnLoad(useGetContactsQuery, contactReducer.isInitialized, { user_id: User._id, Authorization: User.userToken }, initializeContacts, "")
-    return (
-        <>
-            <section className="flex flex-col gap-2">
-                <div className="sticky top-0 z-10 backdrop-blur-3xl">
-                    <h3 className="text-pink-red text-2xl font-semibold p-2">
-                        Contacts
-                    </h3>
-                    <div className="flex justify-between p-[1rem] items-center">
-                        <p>Add new Contact</p>
-                        <i className=" text-pink-red text-2xl bg-blue-gray-100 hover:bg-blue-gray-200 rounded-full p-2 cursor-pointer" onClick={() => setIsModalOpen(true)}>
-                            <AiOutlinePlus />
-                        </i>
-                    </div>
-                    <div className="relative">
-                        <i className="absolute left-2 top-[1.1rem] text-gray-400 "><BsSearch /></i>
-                        <input type="text" name="contact_search" placeholder="Search" className="border inputBorder indent-[0.75rem] w-full p-[0.9rem] rounded-md outline-none" />
-                    </div>
-                    <h1 className="p-3 text-center">List of the all Contacts</h1>
-                </div>
-                <div className=" flex flex-col gap-4 contacts-min-height">
-                    {contactReducer.contacts.map((c) => {
-                        return <Contact contact={c.contact} saved_as={c.saved_as} key={c._id} _id={c._id} />
-                    })}
-                </div>
-                <Navbar />
-            </section>
-            <Add_Contact isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} user_id={User._id} />
-        </>
-    )
+  const [isContactModalOpen, setIsContactModalOpen] = useState(false);
+  const [searchContact, setSearchContact] = useState("");
+  const User = useTypedSelector((selector) => selector.userReducer);
+  const contactReducer = useTypedSelector(
+    (selector) => selector.contactReducer
+  );
+  const filteredContacts = contactReducer.contacts.filter((c) => {
+    return c.saved_as.startsWith(searchContact);
+  });
+  const [contactsLoading] = useDispatchOnLoad(
+    useGetContactsQuery,
+    contactReducer.isInitialized,
+    { user_id: User._id, Authorization: User.userToken },
+    initializeContacts,
+    ""
+  );
+  return (
+    <>
+      <section className="flex flex-col gap-2">
+        <div className="sticky bg-light-gray pb-[2rem] bottom-boder-radius top-0 z-10 backdrop-blur-3xl p-[0.5rem]">
+          <h3 className="text-pink-red text-2xl font-semibold mb-4">
+            Contacts
+          </h3>
+          <div
+            className="flex justify-between items-center bg-blue-gray-100 hover:bg-blue-gray-200 rounded-md p-3 cursor-pointer"
+            onClick={() => setIsContactModalOpen(true)}
+          >
+            <h4 className="text-[18px]">Create new Contact</h4>
+            <i className=" text-pink-red text-2xl">
+              <AiOutlinePlus />
+            </i>
+          </div>
+          <p className="text-center font-[500] my-4 text-pink-red">
+            List of saved contacts
+          </p>
+          <div className="relative">
+            <i className="absolute left-2 top-[1.3rem] text-gray-400">
+              <BsSearch />
+            </i>
+            <input
+              type="text"
+              name="contact_search"
+              placeholder="Search your contacts"
+              value={searchContact}
+              onChange={(e) => setSearchContact(e.target.value)}
+              className="border inputBorder mt-1 indent-[0.75rem] w-full p-[0.9rem] rounded-md outline-none"
+            />
+          </div>
+        </div>
+        <div className=" flex flex-col gap-4 contacts-min-height">
+          {contactReducer.contacts.length == 0 && (
+            <div className="flex justify-center items-center contacts-min-height">
+              <img
+                src={no_data_found}
+                alt={no_data_found}
+                className="min-w-[250px] flex-shrink-0"
+              />
+            </div>
+          )}
+          {searchContact && filteredContacts.length == 0 ? (
+            <div className="flex justify-center items-center contacts-min-height">
+              <p className="text-3xl font-sans ADLaMFont">No Contact Found!</p>
+            </div>
+          ) : (
+            filteredContacts.map((c) => {
+              return (
+                <Contact
+                  contact={c.contact}
+                  saved_as={c.saved_as}
+                  key={c._id}
+                  _id={c._id}
+                />
+              );
+            })
+          )}
+        </div>
+        <Navbar />
+      </section>
+      <Add_Contact
+        isModalOpen={isContactModalOpen}
+        setIsModalOpen={setIsContactModalOpen}
+        user_id={User._id}
+      />
+    </>
+  );
+}
+{
+  /* 
+: */
 }
